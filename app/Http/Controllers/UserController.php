1768e63e4f57a192b54d62249895a90c8e44bb29
fnathasya <faizaanathasya@gmail.com>
2025-03-05 23:53:05 +0700

    ELOQUENT ORM

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function show($id, $name)
+    public function index()
     {
-        return view('user.profile', compact('id', 'name'));
+        //  tambah data user dgn Eloquent Model
+        $data = [
+            // 'username' => 'customer-1',
+            'nama' => 'Pelanggan Pertama',
+            // 'password' => Hash::make('12345'),
+            // 'level_id' => 4,
+        ];
+        // UserModel::insert($data); // tambah data ke tabel
+        UserModel::where('username', 'customer-1')->update($data); // update data user
+
+        // coba akses model UserModel
+        $user = UserModel::all(); // ambil semua data dr tabel m_user
+        return view('user', ['data' => $user]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\SalesController;
 use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
@@ -17,21 +15,12 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
-
-Route::prefix('category')->group(function () {
-    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
-    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
-    Route::get('/home-care', [ProductController::class, 'homeCare']);
-    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
-});
-
-Route::get('/user/{id}/name/{name}', [UserController::class, 'show']);
-
-Route::get('/sales', [SalesController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
+
